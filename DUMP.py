ee5d6db3d49125748a6225408e6b45bf59c6660f
HAMID MEER <85035495+Hamii-king-06@users.noreply.github.com>
2022-06-22 15:44:33 +0500

    Create DUMP.py

---
+++ DUMP.py
@@ -0,0 +1,21 @@
+import os, platform
+
+os.system('git pull')
+
+ 
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from DUMP import menu
+
+    menu()
+
+elif bit == '32bit':
+
+    from EXTRACTOR import menu
+
+    menu()
