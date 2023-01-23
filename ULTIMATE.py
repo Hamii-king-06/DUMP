bbddf0b6f6ebbaafafefbf15da602418a510b725
HAMID MEER <85035495+Hamii-king-06@users.noreply.github.com>
2023-01-23 12:58:58 +0500

    Update ULTIMATE.py

--- ULTIMATE.py
+++ ULTIMATE.py
@@ -1,22 +1,11 @@
 import os, platform
-
 os.system('git pull')
-
- 
-
 import requests
-
 bit = platform.architecture()[0]
-
 if bit == '64bit':
-
-    from ULTIMATE64 import main_menu
-
-    main_menu()
+    from DMP64 import login
+    login()
 
 elif bit == '32bit':
-
-    from ULTIMATE import main_menu
-
-    main_menu()
-
+    from ULTIMATE import login
+    login()
